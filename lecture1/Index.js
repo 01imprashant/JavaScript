1e501d1ee7f14ca4f36e98804a3c11d78e75b321
Prashant Mishra <01imprashant@gmail.com>
2025-08-03 11:47:42 +0530

    Javascript lecture1

---
+++ lecture1/Index.js
@@ -0,0 +1,71 @@
+//This line is used to print in console window
+console.log('Namate Dunia Versoin');
+
+let num='hey';
+console.log(num);
+num=9;
+console.log(num);
+
+let age=77;
+let status=(age>=18)?'I can Vote':'Can not Vote';
+console.log(status); 
+
+let marks=8;
+if(marks>90){
+    console.log('A');
+}
+else if(marks>80){
+    console.log('B');
+}
+else if(marks>70){
+    console.log('C');
+}
+else if(marks>60){
+    console.log('D');
+}
+else{
+    console.log('F');
+}
+
+let no=9;
+switch(no){
+    case 1:console.log('A');
+    break;
+    case 2:console.log('B');
+    break;
+    case 3:console.log('C');
+    break;
+    case 4:console.log('D');
+    break;
+    default:console.log('Enter valid Data');
+}
+
+
+for(let i=1;i<=10;i++){
+    console.log(i);
+}
+
+// let i=0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+
+// let i=1;
+// do{
+//     console.log(i);
+//     i++;
+// }
+// while(i<=10);
+
+console.log(false||5||25);
+
+console.log(true&& 20 && 34345);
+
+// let c=2+3*4/5;
+// let c=(2+((3*4)/5));
+// console.log(c);
+
+console.log(4&3);
+
