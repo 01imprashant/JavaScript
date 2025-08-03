de24643e2a1cd36dcd8ac9ad80fcd03c8bfd6d84
Prashant Mishra <01imprashant@gmail.com>
2025-08-03 11:48:08 +0530

    Javascript lecture3

---
+++ lecture3/Index.js
@@ -0,0 +1,154 @@
+console.log('Chaliaya suru karte hai');
+
+// let lastName='  Babbar  ';
+
+// let firstName=new String('babbar');
+
+// let message=`
+// Hello ${firstName}
+
+// Thanks for the operchunity
+
+// Your Regard
+// Babbar`;
+// console.log(message);
+
+// let date=new Date();
+// console.log(date);
+// let date1=new Date('June 20 2002 9:15')
+// console.log(date1);
+// let date2=new Date(2002,20,7,8);
+// console.log(date2);
+// let words=message.split(' ');
+// console.log(words);
+
+// //creation of array
+// let numbers=[1,3,5,7];
+// console.log(numbers);
+// //insertion in array
+// numbers.push(9);
+// console.log(numbers);
+// numbers.unshift(8);
+// console.log(numbers);
+// numbers.splice(2,0,'a','b','c');
+// console.log(numbers);
+
+// //searching in array
+// console.log(numbers);
+// console.log(numbers.indexOf(1));
+
+// if(numbers.indexOf(4)!=-1){
+//     console.log('Present');
+// }
+// console.log(numbers.includes(71));
+// console.log(numbers.indexOf(1,3));
+
+// let courses=[
+//     {no:1,naam:'Love'},
+//     {no:2,naam:'Babbar'}
+// ];
+
+// console.log(courses);
+
+// console.log(courses.includes({no:1,naam:'Love'}))
+
+// let course=courses.find(function(course){
+//     return course.naam==='Shivam';
+// });
+
+// let course=courses.find(course=>course.naam==='Love');
+
+
+// console.log(course);
+
+// let numbers=[1,2,3,4,5];
+// console.log(numbers);
+// numbers.pop();
+// console.log(numbers);
+// numbers.shift();
+// console.log(numbers);
+// numbers.splice(2,1);
+// console.log(numbers);
+
+
+// let number=[1,2,3,4,5];
+// let number2=number;
+// number=[];
+// // number.length=0;
+// number.splice(0,number.length);
+// console.log(number);
+// console.log(number2);
+
+// let first=[1,2,3];
+// let second=[4,5,6];
+// let combined=first.concat(second);
+// console.log(combined);
+
+// let sliced=combined.slice(2,6);
+// console.log(sliced);
+
+
+// let first=[1,2,3];
+// let second=[4,5,6];
+
+// let combined=[...first,'a',false,...second,'b',true];
+// console.log(combined);
+
+// //copy kaise create kare
+// let another=[...combined];
+// console.log(another);
+
+// let arr=[10,20,30,40,50];
+
+// // itrate by using for of loop
+// for(let value of arr){
+//     console.log(value);
+// }
+
+// //iterate by using forEach loop
+// arr.forEach(number=> console.log(number));
+
+// let number=[10,20,30,40,50];
+
+// let joined=number.join(',');
+// console.log(joined);
+
+// let message='This is my first message';
+
+// let parts=message.split(' ');
+// console.log(parts);
+
+// let joineed=parts.join(' ');
+// console.log(joineed);
+
+
+// let number=[40,10,30,50,20];
+// number.sort();
+// console.log(number);
+// number.reverse();
+// console.log(number);
+
+
+// let number=[1,2,-1,-4];
+
+// let filtered=number.filter(value=>value>=0);
+
+// console.log(filtered);
+
+
+// let number=[1,2,3,4,5,6,];
+// console.log(number);
+
+// let items=number.map(value=>'Student_no'+ value);
+
+// console.log(items);
+
+
+// let numbers=[1,2,-6,-9];
+
+// let filtered=number.filter(value=>value>=0);
+
+// let items=filtered.map(num=>{ value:num});
+// console.log(items);
+
+
